1cc222a9f1008ff27cf016a91fa3a0d171d38656
AlekseyGurzhiy <32543846+AlekseyGurzhiy@users.noreply.github.com>
2021-03-06 14:51:15 +0300

    Update connect_database.php

--- design/connect_database.php
+++ design/connect_database.php
@@ -1,6 +1,6 @@
 <?header('Content-type: text/html; charset=utf-8');
 	//Подключаемся к базе данных
-	$connect = mysql_connect("localhost","alekseyg1","Awedq23");
+	$connect = mysql_connect("localhost","_____","_____");
 	if (!$connect) die("Не могу подсоединиться к базе данных!");
 	$select_database = mysql_select_db("db_alekseyg1");
 	mysql_query("SET NAMES 'UTF8'");
